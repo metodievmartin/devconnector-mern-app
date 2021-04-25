b1313a324b3cf58ab5b15218a52a210474a002ee
Martin Metodiev <martin.metodiew@gmail.com>
2021-04-25 20:03:57 +0300

    Add routes for getting a Github profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const {check, validationResult} = require('express-validator');
+const config = require('config');
+const axios = require('axios');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -322,8 +324,29 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
         res.json(profile);
     } catch (err) {
         console.error(err.message);
-        res.status(500).send('Server error')
+        res.status(500).send('Server error');
     }
-})
+});
+
+// @route   GET api/profile/github/:username
+// @desc    Get user repos from GitHub
+// @access  Public
+router.get('/github/:username', async (req, res) => {
+    try {
+        const uri = encodeURI(
+            `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+        );
+        const headers = {
+            'user-agent': 'node.js',
+            Authorization: `token ${config.get('githubToken')}`
+        };
+
+        const gitHubResponse = await axios.get(uri, { headers });
+        return res.json(gitHubResponse.data);
+    } catch (err) {
+        console.error(err.message);
+        return res.status(404).json({ msg: 'No Github profile found' });
+    }
+});
 
 module.exports = router;
