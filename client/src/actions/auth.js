444ce7e785b7b1d20f4157bb8d1a90df14607f0e
Martin Metodiev <martin.metodiew@gmail.com>
2021-04-27 18:57:42 +0300

    Add Clear profile action

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,7 +6,7 @@ import {
     USER_LOADED,
     AUTH_ERROR,
     LOGIN_SUCCESS,
-    LOGIN_FAIL, LOGOUT
+    LOGIN_FAIL, LOGOUT, CLEAR_PROFILE
 } from "./types";
 import setAuthToken from "../utils/setAuthToken";
 
@@ -98,5 +98,6 @@ export const login = (email, password) => async dispatch => {
 
 // Logout / Clear Profile
 export const logout = () => dispatch => {
+    dispatch({type: CLEAR_PROFILE});
     dispatch({type: LOGOUT});
 };
