d0626930508a0866ab684d7b5490778671b3e392
Martin Metodiev <martin.metodiew@gmail.com>
2021-04-27 13:35:25 +0300

    Implement User logout & Navbar links

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -4,5 +4,6 @@ export const REGISTRATION_SUCCESS = 'REGISTRATION_SUCCESS';
 export const REGISTRATION_FAIL = 'REGISTRATION_FAIL';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const LOGOUT = 'LOGOUT';
 export const USER_LOADED = 'USER_LOADED';
 export const AUTH_ERROR = 'AUTH_ERROR';
