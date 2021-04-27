3c3e9b363533a3c9c94a936c71da5306dfc77915
Martin Metodiev <martin.metodiew@gmail.com>
2021-04-27 16:06:21 +0300

    Add Protected Route component

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -99,5 +99,4 @@ export const login = (email, password) => async dispatch => {
 // Logout / Clear Profile
 export const logout = () => dispatch => {
     dispatch({type: LOGOUT});
-
 };
---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import PropTypes from "prop-types";
+
+const Dashboard = (props) => {
+    return (
+        <div>Dashboard</div>
+    );
+};
+
+Dashboard.propTypes = {};
+
+export default Dashboard;
