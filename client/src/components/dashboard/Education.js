1a3add7d8000c8857fd2816986e31e8f2eb126fc
Martin Metodiev <martin.metodiew@gmail.com>
2021-04-29 20:47:58 +0300

    Implement functionality to delete experience & education

--- client/src/components/dashboard/Education.js
+++ client/src/components/dashboard/Education.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types';
 import Moment from 'react-moment';
 import {connect} from 'react-redux';
 
-const Education = ({education}) => {
+import {deleteEducation} from "../../actions/profile";
+
+const Education = ({education, deleteEducation}) => {
     const educationItems = education.map(edu => (
         <tr key={edu._id}>
             <td>{edu.school}</td>
@@ -14,7 +16,7 @@ const Education = ({education}) => {
             }
             </td>
             <td>
-                <button className="btn btn-danger">Delete</button>
+                <button onClick={() => deleteEducation(edu._id)} className="btn btn-danger">Delete</button>
             </td>
         </tr>
     ));
@@ -41,6 +43,7 @@ const Education = ({education}) => {
 
 Education.propTypes = {
     education: PropTypes.array.isRequired,
+    deleteEducation: PropTypes.func.isRequired,
 };
 
-export default Education;
+export default connect(null, {deleteEducation})(Education);
